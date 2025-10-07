44ff8b4cfd4f2ba5de473dc472fb6cd15d4f706e
Quin F. <quin@cheesits456.dev>
2025-10-07 12:16:45 -0700

    Begin work on the page for making a new log entry

--- page/script.js
+++ page/script.js
@@ -35,6 +35,25 @@ function loadPage(page) {
 		case "new":
 			pageName = "New Log Entry";
 			pageIcon = "pencil";
+			pageContent = `
+				<div class="container">
+					<form class="row">
+						<div class="col-5">
+							<label for="date">Date:</label>
+							<input id="date" class="form-control" type="date" disabled>
+						</div>
+						<div class="col-1">
+							<div class="form-check today-checkbox">
+								<input id="today" class="form-check-input hover-pointer" type="checkbox" checked onclick="toggleDateInput()">
+								<label class="form-check-label hover-pointer" for="today" onclick="toggleDateInput()">Today</label>
+							</div>
+						</div>
+						<div class="col">
+							<input class="form-control" type="text">
+						</div>
+					</form>
+				</div>
+			`;
 			document.getElementById(`navlink-dashboard`).classList.remove("active");
 			document.getElementById(`navlink-new`).classList.add("active");
 			document.getElementById(`navlink-all`).classList.remove("active");
@@ -49,9 +68,21 @@ function loadPage(page) {
 			document.getElementById(`navlink-all`).classList.add("active");
 			break;
 	}
-	
+
 	document.getElementById("page-title").innerHTML = `<i class="fa-solid fa-${pageIcon}"></i> ${pageName}`;
 	document.getElementById("main").innerHTML = pageContent;
+
+	if (page === "new") {
+		const currentDate = new Date();
+		const year = currentDate.getFullYear();
+		let month = currentDate.getMonth() + 1;
+		let day = currentDate.getDate();
+
+		if (`${month}`.length === 1) month = `0${month}`;
+		if (`${day}`.length === 1) day = `0${day}`;
+
+		document.getElementById("date").value = `${year}-${month}-${day}`;
+	}
 }
 
 
@@ -62,4 +93,25 @@ function setCopyrightInfo() {
 
 	document.getElementById("version-number").innerText = versionNumber;
 	document.getElementById("year").innerText = currentYear;
+}
+
+
+
+function toggleDateInput() {
+	const checked = document.getElementById("today").checked;
+	if (checked) {
+		document.getElementById("date").setAttribute("disabled", true);
+
+		const currentDate = new Date();
+		const year = currentDate.getFullYear();
+		let month = currentDate.getMonth() + 1;
+		let day = currentDate.getDate();
+
+		if (`${month}`.length === 1) month = `0${month}`;
+		if (`${day}`.length === 1) day = `0${day}`;
+
+		document.getElementById("date").value = `${year}-${month}-${day}`;
+	} else {
+		document.getElementById("date").removeAttribute("disabled");
+	};
 }
