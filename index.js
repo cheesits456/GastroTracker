bdde56a505549244c77da2c93688a40bdc041f2a
Quin F. <quin@cheesits456.dev>
2025-10-08 23:50:07 -0700

    Set window minimum height and width

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function createWindow() {
 	const window = new electron.BrowserWindow({
 		width: 1200,
 		height: 650,
+		minWidth: 1200,
+		minHeight: 650,
 		icon: path.join(__dirname, "icon", "icon.png"),
 		webPreferences: {
 			contextIsolation: false,
